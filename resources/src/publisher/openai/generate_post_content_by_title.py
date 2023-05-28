8ced7643cc3479b35bc8796bd86efe42796539b1
Alex Santeeno <alexsanteeno@gmail.com>
2023-05-28 17:23:59 +0200

    switch to chat gpt

--- resources/src/publisher/openai/generate_post_content_by_title.py
+++ resources/src/publisher/openai/generate_post_content_by_title.py
@@ -6,10 +6,15 @@ import string
 def generate_post_content_by_title(title: string):
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    content = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Create a seo optimized article with minimum 1500 characters about " + title + "",
-        max_tokens=4000,
-        temperature=0.8
+
+    content = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Create a seo optimized article with minimum 1500 characters about " + title + ""}
+            ]
     )
-    return content.choices[0].text
+
+
+    print(content)
+    return content.choices[0].message.content
